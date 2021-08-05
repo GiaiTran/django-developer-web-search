826b2c4ce20669cd97ed18ffdf9428d9d7bc86a0
GiaiTran <39039933+GiaiTran@users.noreply.github.com>
2021-08-05 17:56:24 -0500

    Add files via upload

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
     path("", include("users.urls")),
+    path("api/", include("api.urls")),
+
     # Password reset
     path(
         "reset_password/",
